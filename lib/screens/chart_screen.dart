17c16d314406fe089686c1657787cd0a8538bb67
modather <modather0ali@gmail.com>
2022-08-15 10:30:26 +0200

    new update

--- lib/screens/chart_screen.dart
+++ lib/screens/chart_screen.dart
@@ -1,9 +1,8 @@
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 
-/// !!Step1: prepare the data to plot.
 // final _data1 = <double, double>{1: 10, 2: 15, 3: 20, 4: 28, 5: 34, 6: 50};
-final _data2 = <double, double>{
+final _outCome = <double, double>{
   1: 8,
   2: 12,
   3: 27,
@@ -18,6 +17,50 @@ final _data2 = <double, double>{
   12: 35,
   13: 55,
   14: 10,
+  15: 8,
+  16: 12,
+  17: 27,
+  18: 31,
+  19: 36,
+  21: 33,
+  22: 50,
+  23: 30,
+  24: 5,
+  25: 25,
+  26: 35,
+  27: 55,
+  29: 10,
+  30: 42,
+};
+final _inCome = <double, double>{
+  1: 8,
+  2: 50,
+  3: 0,
+  4: 22,
+  5: 0,
+  6: 0,
+  7: 40,
+  8: 60,
+  9: 0,
+  10: 5,
+  11: 0,
+  12: 10,
+  13: 55,
+  14: 20,
+  15: 0,
+  16: 40,
+  17: 10,
+  18: 75,
+  19: 50,
+  21: 0,
+  22: 0,
+  23: 0,
+  24: 5,
+  25: 15,
+  26: 0,
+  27: 0,
+  29: 0,
+  30: 0,
 };
 
 class ChartScreen extends StatefulWidget {
@@ -28,48 +71,43 @@ class ChartScreen extends StatefulWidget {
 }
 
 class _ChartScreenState extends State<ChartScreen> {
-  bool _showGrid = true;
-  bool _isCurved = false;
+  bool _showIncome = true;
+  bool _showOutcome = true;
+
   bool _showBelowArea = false;
   bool _showDot = true;
-  bool _showBorder = true;
 
   @override
   Widget build(BuildContext context) {
-    /// !!Step2: convert data into a list of [FlSpot].
-    // final spots1 = <FlSpot>[
-    //   for (final entry in _data1.entries) FlSpot(entry.key, entry.value)
-    // ];
+    final spots1 = <FlSpot>[
+      for (final entry in _inCome.entries) FlSpot(entry.key, entry.value)
+    ];
     final spots2 = <FlSpot>[
-      for (final entry in _data2.entries) FlSpot(entry.key, entry.value)
+      for (final entry in _outCome.entries) FlSpot(entry.key, entry.value)
     ];
 
-    /// !!Step3: prepare LineChartData
-    /// !here we can set styles and behavior of the chart.
     final lineChartData = LineChartData(
       maxX: 30,
       maxY: 80,
       lineBarsData: [
-        // ! Here we can style each data line.
-        // LineChartBarData(
-        //   spots: spots1,
-        //   color: Colors.blue,
-        //   barWidth: 8,
-        //   isCurved: _isCurved,
-        //   dotData: FlDotData(show: _showDot),
-        //   belowBarData:
-        //       BarAreaData(show: _showBelowArea, color: Colors.blue[200]),
-        // ),
         LineChartBarData(
+          spots: spots1,
+          color: Colors.blue,
+          show: _showIncome,
+          dotData: FlDotData(show: _showDot),
+          belowBarData:
+              BarAreaData(show: _showBelowArea, color: Colors.blue[200]),
+        ),
+        LineChartBarData(
+          show: _showOutcome,
           aboveBarData: BarAreaData(color: Colors.green),
           showingIndicators: [1, 2, 3],
           spots: spots2,
           color: Colors.red,
-          barWidth: 4,
           isCurved: true,
           dotData: FlDotData(show: _showDot),
-          // belowBarData:
-          //     BarAreaData(show: _showBelowArea, color: Colors.red[200]),
+          belowBarData: BarAreaData(
+              show: _showBelowArea, color: Colors.red.withOpacity(0.3)),
         ),
       ],
       // ! Behavior when touching the chart:
@@ -105,8 +143,6 @@ class _ChartScreenState extends State<ChartScreen> {
           axisNameWidget: const Text('Value'),
           sideTitles: SideTitles(
             showTitles: true,
-            // ! Decides how to show left titles,
-            // here we skip some values by returning ''.
             getTitlesWidget: (double val, _) {
               if (val.toInt() % 5 != 0) return const Text('');
               return Text('${val.toInt()}');
@@ -115,57 +151,73 @@ class _ChartScreenState extends State<ChartScreen> {
         ),
       ),
     );
-    return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(10),
-        child: ListView(
-          scrollDirection: Axis.horizontal,
-          children: [
-            SizedBox(
-              height: MediaQuery.of(context).size.height,
-              width: 1000,
-              child: LineChart(lineChartData),
-            ),
-          ],
+    return SafeArea(
+      child: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.all(10),
+          child: ListView(
+            scrollDirection: Axis.horizontal,
+            children: [
+              SizedBox(
+                height: MediaQuery.of(context).size.height,
+                width: 1000,
+                child: LineChart(lineChartData),
+              ),
+            ],
+          ),
         ),
+        bottomNavigationBar: _buildControlWidgets(),
       ),
-      // bottomNavigationBar: _buildControlWidgets(),
     );
   }
 
   Widget _buildControlWidgets() {
     return Container(
-      height: 200,
+      height: 60,
       color: Colors.grey[200],
       child: ListView(
+        scrollDirection: Axis.horizontal,
+        padding: const EdgeInsets.only(bottom: 10, right: 10, left: 10),
         children: [
-          SwitchListTile(
-            title: const Text('Curved'),
-            onChanged: (bool val) => setState(() => _isCurved = val),
-            value: _isCurved,
+          _switchRow(
+            title: "Show Income",
+            switchValue: _showIncome,
+            onChanged: (bool val) => setState(() => _showIncome = val),
           ),
-          SwitchListTile(
-            title: const Text('ShowGrid'),
-            onChanged: (bool val) => setState(() => _showGrid = val),
-            value: _showGrid,
+          _switchRow(
+            title: "Show Outcome",
+            switchValue: _showOutcome,
+            onChanged: (bool val) => setState(() => _showOutcome = val),
           ),
-          SwitchListTile(
-            title: const Text('ShowBorder'),
-            onChanged: (bool val) => setState(() => _showBorder = val),
-            value: _showBorder,
-          ),
-          SwitchListTile(
-            title: const Text('ShowBelowArea'),
+          _switchRow(
+            title: "Show Below Area",
+            switchValue: _showBelowArea,
             onChanged: (bool val) => setState(() => _showBelowArea = val),
-            value: _showBelowArea,
           ),
-          SwitchListTile(
-            title: const Text('ShowDot'),
+          _switchRow(
+            title: "Show Dot",
+            switchValue: _showDot,
             onChanged: (bool val) => setState(() => _showDot = val),
-            value: _showDot,
           ),
         ],
       ),
+    );
+  }
+
+  Widget _switchRow({
+    required String title,
+    required bool switchValue,
+    required void Function(bool)? onChanged,
+  }) {
+    return Row(
+      children: [
+        Text(
+          title,
+          style: const TextStyle(fontWeight: FontWeight.w600),
+        ),
+        Switch(value: switchValue, onChanged: onChanged),
+        const SizedBox(width: 10),
+      ],
     );
   }
 }
